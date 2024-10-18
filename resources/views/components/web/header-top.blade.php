e963de5c0b98303d8a96a9a6617abdf16dbcc5a5
ACHii <>
2024-10-18 12:17:49 +0530

    header top

---
+++ resources/views/components/web/header-top.blade.php
@@ -0,0 +1,21 @@
+<div class="bg-gray-50 w-full">
+    <div class="max-w-screen-xl mx-auto px-4 py-2">
+        <div class="flex md:justify-between items-center justify-center">
+            <div class="shrink-0 text-sm text-gray-500">Welcome to Flowbite!</div>
+            <ul class="items-center divide-x divide-solid text-sm text-gray-500 md:flex hidden">
+                <li>
+                    <a href="/faq"
+                       class="px-4 hover:underline">FAQs</a>
+                </li>
+                <li>
+                    <a href="/about"
+                       class="px-4 hover:underline">About Us</a>
+                </li>
+                <li>
+                    <a href="/contact"
+                       class="px-4 hover:underline">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
