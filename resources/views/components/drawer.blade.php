62aa45807b329985454d885b0b0f34c7dc75fe99
ACHii <>
2024-10-18 13:42:08 +0530

    drawer component

---
+++ resources/views/components/drawer.blade.php
@@ -0,0 +1,26 @@
+@props(['id'])
+
+<div id="{{ $id }}"
+     {{ $attributes->merge(['class' => 'fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80']) }}
+     tabindex="-1">
+    <div class="flex items-center justify-between gap-4">
+        <a href="#"
+           class="flex items-center space-x-3 shrink-0">
+            <img src="https://flowbite.com/docs/images/logo.svg"
+                 class="h-8"
+                 alt="Flowbite Logo">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+        </a>
+
+        <x-button type="button"
+                  variant="icon"
+                  data-drawer-hide="{{ $id }}"
+                  aria-controls="{{ $id }}"
+                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-600">
+            <x-lucide-x class="w-5 h-5" />
+            <span class="sr-only">Close menu</span>
+        </x-button>
+    </div>
+
+    {{ $slot }}
+</div>
