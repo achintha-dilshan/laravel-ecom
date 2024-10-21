e5498ecd2310d0c9f83fa6ad48cae7aa1d46c858
ACHii <>
2024-10-21 12:49:16 +0530

    cart dropdown item

--- resources/views/components/web/cart-dropdown-item.blade.php
+++ resources/views/components/web/cart-dropdown-item.blade.php
@@ -1,16 +1,17 @@
-<li class="flex gap-4">
+<li class="flex gap-4 p-2 hover:bg-gray-100 rounded-lg transition ease-in-out duration-150">
     <a href="#"
-       class="size-12 aspect-square shrink-0">
-        <img src="https://picsum.photos/200"
-             class="w-full h-full object-cover rounded-md"
-             loading="lazy"
-             alt="Product Image">
+       class="flex gap-4">
+        <div class="size-12 aspect-square shrink-0">
+            <img src="https://picsum.photos/200"
+                 class="w-full h-full object-cover rounded-md"
+                 loading="lazy"
+                 alt="Product Image">
+        </div>
+        <div>
+            <h5 class="text-gray-700 line-clamp-1">Lorem ipsum dolor sit amet consectetur</h5>
+            <span class="text-sm text-gray-500">2 x Rs. 1990.99</span>
+        </div>
     </a>
-    <div>
-        <a href="#"
-           class="text-gray-700 line-clamp-1">Lorem ipsum dolor sit amet consectetur</a>
-        <span class="text-sm text-gray-500">2 x Rs. 1990.99</span>
-    </div>
     <div class="shrink-0">
         <x-button type="button"
                   variant="icon"
