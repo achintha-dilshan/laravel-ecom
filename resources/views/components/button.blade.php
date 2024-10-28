0d1733d3d00ae219faf8b07ade9e169fe1e5bc80
ACHii <>
2024-10-28 11:25:48 +0530

    button fix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -6,19 +6,23 @@
 
     $classes =
         $defaultClasses .
-        ' px-4 py-2 bg-primary-700 border border-transparent rounded-md text-white hover:bg-primary-800 focus:bg-primary-800 active:bg-primary-900';
+        ' px-5 py-2.5 bg-primary-700 border border-primary-700 hover:border-primary-800 rounded-md text-white hover:bg-primary-800 focus:bg-primary-800';
 
     if (strtolower($variant) == 'secondary') {
         $classes =
-            $defaultClasses . ' px-4 py-2 bg-white border border-gray-300 rounded-md text-gray-700 hover:bg-gray-50';
+            $defaultClasses . ' px-5 py-2.5 bg-white border border-gray-300 rounded-md text-gray-700 hover:bg-gray-50';
     }
 
     if (strtolower($variant) == 'soft') {
-        $classes = $defaultClasses . ' px-4 py-2 bg-gray-100 rounded-md text-gray-700 hover:bg-gray-200';
+        $classes =
+            $defaultClasses .
+            ' px-5 py-2.5 bg-gray-100 rounded-md border border-gray-100 hover:border-gray-200 text-gray-700 hover:bg-gray-200';
     }
 
     if (strtolower($variant) == 'icon') {
-        $classes = $defaultClasses . ' p-2 bg-white rounded-full text-gray-700 hover:bg-gray-100';
+        $classes =
+            $defaultClasses .
+            ' p-2 bg-white rounded-full text-gray-700 hover:bg-gray-100 border border-transparent hover:border-gray-100';
     }
 @endphp
 
