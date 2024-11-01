ee044d946aed0e2fd0fd62f6e7554a183001b015
ACHii <>
2024-11-01 16:06:08 +0530

    category pill

---
+++ resources/views/web/components/category-pill.blade.php
@@ -0,0 +1,2 @@
+<a
+   {{ $attributes->merge(['class' => 'inline-block px-4 py-2 m-1 text-sm font-semibold text-gray-500 transition duration-150 ease-in-out bg-white border rounded-full hover:border-primary-200', 'href' => 'javascript:void(0)']) }}>{{ $slot }}</a>
