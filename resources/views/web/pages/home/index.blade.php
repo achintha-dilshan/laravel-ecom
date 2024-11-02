a3f8a7ea7aac322ad26a6d1595fb25a8076f6fc6
ACHii <>
2024-11-03 00:00:57 +0530

    category section

--- resources/views/web/pages/home/index.blade.php
+++ resources/views/web/pages/home/index.blade.php
@@ -1,7 +1,19 @@
 <x-web::layout>
+    {{-- hero section --}}
     @include('web.pages.home.partials.hero-slider')
 
+    {{-- category section --}}
     <section class="my-10">
+        <x-web::section-title title="Browse Categories"
+                              targetTitle="Shop Now" />
+
+        {{-- category slider --}}
+        @include('web.pages.home.partials.category-slider')
+    </section>
+
+    <section class="my-10">
+        <x-web::section-title title="New Arrivals"
+                              targetTitle="View More" />
         <div class="flex items-start gap-4">
             <x-web::product-card />
         </div>
---
+++ resources/views/web/pages/home/partials/category-slider.blade.php
@@ -0,0 +1,39 @@
+<x-swiper.container class="category-slider"
+                    data-config="{'slidesPerView': 'auto','freeMode': true,'autoplay': { 'delay': 1, 'disableOnInteraction': false },'speed': 10000}">
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Electronics</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Phones & Communication</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Computer & Accessories</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Sports, Fiteness & Entertainment</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Home & Kitchen</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Home Improvement & Tools</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Toys & Kids</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Toys & Kids</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Toys & Kids</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Toys & Kids</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Toys & Kids</x-web::category-pill>
+    </x-swiper.slide>
+    <x-swiper.slide class="!w-fit">
+        <x-web::category-pill>Toys & Kids</x-web::category-pill>
+    </x-swiper.slide>
+</x-swiper.container>
