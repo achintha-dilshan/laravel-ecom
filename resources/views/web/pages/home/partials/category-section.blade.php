19187d6332dbb27d0a4baebeff26d55429ef7add
ACHii <>
2024-11-03 15:30:17 +0530

    category section

---
+++ resources/views/web/pages/home/partials/category-section.blade.php
@@ -0,0 +1,45 @@
+<section class="my-10">
+    <x-web::section-title title="Browse Categories"
+                          targetTitle="Shop Now" />
+
+    {{-- category slider --}}
+    <x-swiper.container class="category-slider"
+                        data-config="{'slidesPerView': 'auto','freeMode': true,'autoplay': { 'delay': 1, 'disableOnInteraction': false },'speed': 10000}">
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Electronics</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Phones & Communication</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Computer & Accessories</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Sports, Fiteness & Entertainment</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Home & Kitchen</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Home Improvement & Tools</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Toys & Kids</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Toys & Kids</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Toys & Kids</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Toys & Kids</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Toys & Kids</x-web::category-pill>
+        </x-swiper.slide>
+        <x-swiper.slide class="!w-fit">
+            <x-web::category-pill>Toys & Kids</x-web::category-pill>
+        </x-swiper.slide>
+    </x-swiper.container>
+</section>
