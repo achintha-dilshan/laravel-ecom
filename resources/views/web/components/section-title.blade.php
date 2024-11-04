b25ef6f312008f827afd5b2df0fbda7afe41560b
ACHii <>
2024-11-04 17:01:04 +0530

    section title

--- resources/views/web/components/section-title.blade.php
+++ resources/views/web/components/section-title.blade.php
@@ -1,8 +1,10 @@
 @props(['title', 'targetTitle', 'href'])
 
-<div class="py-2.5 mb-5 border-b border-gray-200">
+<div class="py-2.5 mb-5 border-b-2 border-gray-200">
     <div class="flex flex-col items-start justify-between gap-1 sm:flex-row">
-        <h2 class="text-2xl font-bold text-gray-600">{{ $title }}</h2>
+        <h2
+            class="relative text-2xl font-bold text-gray-600  after:content-[''] after:absolute after:h-[2px] after:w-[110%] after:bg-gray-400 after:left-0 sm:after:-bottom-3 after:-bottom-[46px]">
+            {{ $title }}</h2>
         <x-web::section-button href="{{ $href ?? 'javascript:void(0)' }}">{{ $targetTitle }}</x-web::section-button>
     </div>
 </div>
